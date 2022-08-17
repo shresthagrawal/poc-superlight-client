da971d74f1d6dea8297efa9cc7d8b2042519fad9
Joachim Neu <email@joachim-neu.de>
2022-08-17 03:26:49 -0700

    Measure energy consumption

---
+++ implementation/run-energy-01.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ve
+
+snapshot() {
+    mkdir -p results/energy-01/$1
+    cp -v /sys/class/power_supply/BAT0/{capacity,capacity_level,charge_full,charge_full_design,charge_now,current_now,voltage_now,voltage_min_design} results/energy-01/$1/
+    date > results/energy-01/$1/date.txt
+    acpi --everything > results/energy-01/$1/acpi.txt
+    dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower/devices/battery_BAT0 org.freedesktop.UPower.Device.Refresh
+    upower -i /org/freedesktop/UPower/devices/battery_BAT0 > results/energy-01/$1/upower.txt
+}
+
+
+./node_modules/.bin/yarn build
+rm -rfv results/energy-01
+
+
+snapshot "01_start"
+
+sleep 600;
+
+snapshot "02_waited"
+
+TRIALS=10 TREE_DEGREE=75 BATCH_SIZE=50 CHAIN_SIZE=3650 SUPERLIGHT=true node dist/benchmark/multiple-prover-optimal-params.js
+
+snapshot "03_slced"
+
+TRIALS=10 TREE_DEGREE=75 BATCH_SIZE=50 CHAIN_SIZE=3650 SUPERLIGHT=false node dist/benchmark/multiple-prover-optimal-params.js
+
+snapshot "04_lced"
+
+sleep 600;
+
+snapshot "05_done"
